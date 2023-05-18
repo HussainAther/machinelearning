8895b6dbc7287dd7da4094cd8b81b46ba995cae6
Syed Hussain Ather <shussainather@gmail.com>
2023-05-18 12:52:14 -0400

    initial commit

---
+++ languagemodel/validation.py
@@ -0,0 +1,13 @@
+from sklearn.model_selection import train_test_split
+
+# vocabulary size
+vocab = len(mapping)
+sequences = np.array(sequences)
+# create X and y
+X, y = sequences[:,:-1], sequences[:,-1]
+# one hot encode y
+y = to_categorical(y, num_classes=vocab)
+# create train and validation sets
+X_tr, X_val, y_tr, y_val = train_test_split(X, y, test_size=0.1, random_state=42)
+
+print('Train shape:', X_tr.shape, 'Val shape:', X_val.shape)
