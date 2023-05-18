06bc1cf733306fd567d2a23a9246a6f05a8601e2
Syed Hussain Ather <shussainather@gmail.com>
2023-05-18 12:52:37 -0400

    define model

--- languagemodel/validation.py
+++ languagemodel/validation.py
@@ -11,3 +11,15 @@ y = to_categorical(y, num_classes=vocab)
 X_tr, X_val, y_tr, y_val = train_test_split(X, y, test_size=0.1, random_state=42)
 
 print('Train shape:', X_tr.shape, 'Val shape:', X_val.shape)
+
+# define model
+model = Sequential()
+model.add(Embedding(vocab, 50, input_length=30, trainable=True))
+model.add(GRU(150, recurrent_dropout=0.1, dropout=0.1))
+model.add(Dense(vocab, activation='softmax'))
+print(model.summary())
+
+# compile the model
+model.compile(loss='categorical_crossentropy', metrics=['acc'], optimizer='adam')
+# fit the model
+model.fit(X_tr, y_tr, epochs=100, verbose=2, validation_data=(X_val, y_val))
