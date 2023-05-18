5967eb0cd103ed385fc672f7cfe7230e17c73757
Syed Hussain Ather <shussainather@gmail.com>
2023-05-18 12:49:04 -0400

    init

---
+++ languagemodel/seqpredict.py
@@ -0,0 +1,13 @@
+def create_seq(text):
+    length = 30
+    sequences = list()
+    for i in range(length, len(text)):
+        # select sequence of tokens
+        seq = text[i-length:i+1]
+        # store
+        sequences.append(seq)
+    print('Total Sequences: %d' % len(sequences))
+    return sequences
+
+# create sequences   
+sequences = create_seq(data_new)
